ed2cc38de8a86bd759d179ad1a414509de1bbdca
kompetch-n <kompetch.n@gmail.com>
2026-01-30 11:54:50 +0700

    leave send line

--- api/models/leave.py
+++ api/models/leave.py
@@ -1,6 +1,12 @@
 from pydantic import BaseModel, Field
-from typing import Optional
-from datetime import datetime
+from typing import Optional, List
+from datetime import datetime, date
+
+
+class ReplacementDoctor(BaseModel):
+    doctor_id: str
+    doctor_name: str
+    status: str = "pending"
 
 
 class LeaveRequest(BaseModel):
@@ -11,13 +17,12 @@ class LeaveRequest(BaseModel):
     department: str
     sub_department: str
 
-    replacement_doctor_id: Optional[str] = None
-    replacement_doctor_name: Optional[str] = None
+    replacement_doctors: List[ReplacementDoctor]
 
     leave_type: str
-    start_date: str
-    end_date: str
+    start_date: date
+    end_date: date
     reason: Optional[str] = None
 
     status: Optional[str] = "pending"
-    created_at: Optional[datetime] = datetime.utcnow()
+    created_at: Optional[datetime] = None
